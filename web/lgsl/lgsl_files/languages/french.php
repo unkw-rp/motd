a93fc4ca499a9f2ceb7c6d81e8f23b859f6571c5
unkw-rp <raul.paiva2016@outlook.com>
2025-03-25 22:56:30 -0300

    eae

---
+++ web/lgsl/lgsl_files/languages/french.php
@@ -0,0 +1,85 @@
+<?php
+
+//------------------------------------------------------------------------------------------------------------+
+
+  global $lgsl_config;
+
+  $lgsl_config['text']['ttl'] = "Live Game Server List";
+  $lgsl_config['text']['vsd'] = "CLIQUER POUR VOIR LES DETAILS";
+  $lgsl_config['text']['slk'] = "REJOINDRE LE SERVEUR";
+  $lgsl_config['text']['sts'] = "Statut";
+  $lgsl_config['text']['adr'] = "Adresse";
+  $lgsl_config['text']['cpt'] = "Connection Port";
+  $lgsl_config['text']['qpt'] = "Query Port";
+  $lgsl_config['text']['typ'] = "Type";
+  $lgsl_config['text']['gme'] = "Jeu";
+  $lgsl_config['text']['map'] = "Carte";
+  $lgsl_config['text']['plr'] = "Joueurs";
+  $lgsl_config['text']['dtl'] = "Details";
+  $lgsl_config['text']['npi'] = "PAS D'INFO JOUEUR";
+  $lgsl_config['text']['nei'] = "PAS D'INFO EXTRA";
+  $lgsl_config['text']['ehs'] = "Paramètres";
+  $lgsl_config['text']['ehv'] = "Valeur";
+  $lgsl_config['text']['onl'] = "EN LIGNE";
+  $lgsl_config['text']['onp'] = "EN LIGNE AVEC MOT DE PASSE";
+  $lgsl_config['text']['nrs'] = "PAS DE RÉPONSE";
+  $lgsl_config['text']['pen'] = "EN ATTENTE DE REQUÊTE";
+  $lgsl_config['text']['zpl'] = "JOUEURS:";
+  $lgsl_config['text']['mid'] = "ID SERVEUR INVALIDE";
+  $lgsl_config['text']['nnm'] = "--";
+  $lgsl_config['text']['nmp'] = "--";
+  $lgsl_config['text']['tns'] = "Serveurs";
+  $lgsl_config['text']['tnp'] = "Joueurs";
+  $lgsl_config['text']['tmp'] = "Joueurs Max";
+  $lgsl_config['text']['asd'] = "AJOUT PUBLIQUE DE SERVEUR DÉSACTIVÉ";
+  $lgsl_config['text']['awm'] = "CETTE ZONE VOUS PERMET DE TESTER ET ENSUITE D'AJOUTER DES SERVEURS EN LIGNE À LA LISTE";
+  $lgsl_config['text']['ats'] = "Tester Serveur";
+  $lgsl_config['text']['aaa'] = "SERVEUR DÉJÀ AJOUTÉ EN ATTENTE D'APPROBATION D'UN ADMIN";
+  $lgsl_config['text']['aan'] = "SERVEUR DÉJÀ AJOUTÉ";
+  $lgsl_config['text']['anr'] = "PAS DE RÉPONSE - VEUILLEZ ÊTRE SÛR D'AJOUTER LES BONNES INFORMATIONS";
+  $lgsl_config['text']['ada'] = "SERVEUR AJOUTÉ POUR APPROBATION D'UN ADMIN";
+  $lgsl_config['text']['adn'] = "SERVEUR AJOUTÉ";
+  $lgsl_config['text']['asc'] = "RÉUSSI - VEUILLEZ CONFIRMER QU'IL S'AGIT DU BON SERVEUR";
+  $lgsl_config['text']['aas'] = "Ajouter un serveur";
+  $lgsl_config['text']['loc'] = "Localisation:";
+  $lgsl_config['text']['cts'] = "Click to show server banners";
+  $lgsl_config['text']['lst'] = "Last update";
+  $lgsl_config['text']['nam'] = "Name";
+  $lgsl_config['text']['gam'] = "Game";
+  $lgsl_config['text']['sta'] = "Status";
+  $lgsl_config['text']['sco'] = "Score";
+  $lgsl_config['text']['tim'] = "Time";
+  $lgsl_config['text']['tea'] = "Team";
+  $lgsl_config['text']['cou'] = "Country";
+  $lgsl_config['text']['pin'] = "Ping";
+  $lgsl_config['text']['dea'] = "Deaths";
+  $lgsl_config['text']['hea'] = "Health";
+  $lgsl_config['text']['pag'] = "Page";
+  $lgsl_config['text']['bak'] = "RETOUR À LA LISTE DES SERVEURS";
+  $lgsl_config['text']['mpg'] = "TO MAIN PAGE";
+  /* admin section */
+  $lgsl_config['text']['tzn'] = "d/m/Y H:i:s";
+  $lgsl_config['text']['umn'] = "NOM D'UTILISATEUR";
+  $lgsl_config['text']['pwd'] = "PASSWORD";
+  $lgsl_config['text']['lgn'] = "Connexion";
+  $lgsl_config['text']['skc'] = "Enregistrer - Conserver le cache";
+  $lgsl_config['text']['srh'] = "Enregistrer - Réinitialiser le cache";
+  $lgsl_config['text']['mip'] = "Chemins d'image de carte";
+  $lgsl_config['text']['avm'] = "Gestion avancée";
+  $lgsl_config['text']['nrm'] = "Gestion normale";
+  $lgsl_config['text']['upd'] = "Updates";
+  $lgsl_config['text']['yes'] = "OUI";
+  $lgsl_config['text']['nno'] = "NO";
+  $lgsl_config['text']['enb'] = "Activé";
+  $lgsl_config['text']['dsb'] = "Disabled";
+  $lgsl_config['text']['aum'] = "ADMIN USERNAME OR PASSWORD MISSING FROM CONFIG";
+  $lgsl_config['text']['apc'] = "ADMIN PASSWORD MUST BE CHANGED FROM THE DEFAULT";
+  $lgsl_config['text']['faq'] = "- To remove a server, delete the IP, then click Save.<br /><br />- Leave the query port blank to have LGSL try to fill it in for you.<br /><br />- Software port is only needed for a few games so it being set 0 is normal.<br /><br />- Edit the lgsl_config.php to set the style and other options.<br /><br />- Keep your LGSL up to date.";
+  $lgsl_config['text']['fso'] = "Required for direct querying of servers";
+  $lgsl_config['text']['crl'] = "Used for the feed when fsockopen is disabled";
+  $lgsl_config['text']['mbs'] = "Used to show UTF-8 server and player names correctly";
+  $lgsl_config['text']['bz2'] = "Used to show Source server settings over a certain size";
+  $lgsl_config['text']['gd2'] = "Required for Image Mod";
+  $lgsl_config['text']['zli'] = "Required for America's Army 3";
+
+//------------------------------------------------------------------------------------------------------------+
---
+++ web/mapend.php
@@ -0,0 +1,195 @@
+<?php
+require_once 'inc/func.php';
+require_once 'geoip2.phar';
+use GeoIp2\Database\Reader;
+
+$style = isset($_GET['style']) ? $_GET['style'] : '';
+$player = isset($_GET['player']) ? $_GET['player'] : '';
+
+$mapend_css = $style ? 'css/mapend.css' : 'css/mapend_nonsteam.css';
+$sql_user = DB::run('SELECT `Map Kills`, `Map Deaths`, `Map XP` FROM '.htmlspecialchars($_GET['db_table3']).' WHERE Player = ?', [$player]);
+$user = $sql_user->fetch(PDO::FETCH_ASSOC);
+
+$sql_mapinfo = DB::run('SELECT `Map Name`, `Team Win` FROM '.htmlspecialchars($_GET['db_table3']).'');
+$mapstats = $sql_mapinfo->fetch();
+
+$sql_id = DB::run('SELECT Player FROM '.htmlspecialchars($_GET['db_table1']).' WHERE Player = ?', [$player]);
+$id = $sql_id->fetch();
+
+$team = '7';
+$team_name = 'Match Draw';
+
+if(isset($mapstats['Team Win'])) {
+	switch($mapstats['Team Win']) {
+	    case 0:
+	        $team = 5;
+	        $team_name = 'Zombies Win';
+	        break;
+	    case 1:
+	        $team = 6;
+	        $team_name = 'Humans Win';
+	        break;
+	    case 2:
+	        $team = 7;
+	        $team_name = 'Match Draw';
+	        break;
+	}
+}
+
+echo '
+<!DOCTYPE html>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="'.$mapend_css.'" />
+<table id="t2">
+    <tr>
+        <th id="th1">
+            Match End Statistics
+            <hr id="hr1" />
+            </hr>
+            <tr>
+                <th id="th2">
+                    Map:
+                    <div id="th3">
+                        '.(isset($mapstats['Map Name']) ? $mapstats['Map Name'] : 'n/a').'
+                    </div>
+                    <hr id="hr0"></hr>
+                </th>
+            </tr>
+        </th>
+    </tr>
+</table>
+<table id="t4">
+    <tr>
+        <th id="th'.$team.'">'.$team_name.'</th>
+    </tr>
+</table>
+<table id="t1">
+    <tr>
+        <th>#</th>
+        <th id="t">Top Players of the Match</th>
+        <th id="k"></th>
+        <th id="d"></th>
+        <th id="kd"></th>
+        <th id="m"></th>
+        <th id="r">Rank</th>
+    </tr>';
+
+$sql_mapstats = DB::run('SELECT `Player`, Nick, `Steam ID`, IP, `Map Kills`, `Map Deaths`, `Map Infections`, `Map XP`, Level, Flags, Online, New, Steam, Avatar FROM '.htmlspecialchars($_GET['db_table3']).' ORDER BY `Map Kills` DESC, `Map Deaths` ASC, `Map Infections` DESC LIMIT 5')->FetchAll(PDO::FETCH_ASSOC);
+$i = 1;
+$table = false;
+
+foreach ($sql_mapstats as $mapstats) { 
+    if ($table) {
+        $tr = '<tr id="b">';
+        $table = false;
+    } else {
+        $tr = '<tr>';
+        $table = true;
+    }
+    if ($id['Player'] == $mapstats['Player']) {
+        $tr = '<tr id="i">';
+    }
+
+	switch($i) {
+		case 1: $td = '<td id=z></td>'; break;
+		case 2: $td = '<td id=w></td>'; break;
+		case 3: $td = '<td id=y></td>'; break;
+		default: $td = '<td id=p>'.$i.'</td>'; break;
+	}
+
+	$avatar = '';
+	$steam = '';
+	$new = '';
+	$color = $default_name_color;
+	$level = $mapstats['Level'] + 1;
+	$reader = new Reader('GeoLite2-City.mmdb');
+
+	try {
+	    $record = $reader->city($mapstats['IP']);
+	    $flag = strtolower($record->country->isoCode);
+	} catch (GeoIp2\Exception\AddressNotFoundException $e) {
+	    $flag = 'nn';
+	}
+	
+	if($mapstats['Steam']) {
+		$steam = '<img src="'.$main_url.'css/img/icon-steam.png" id=steam></img>';
+		$avatar = '<img src="'.$mapstats['Avatar'].'" width=15px></img>';
+	}
+
+	if($mapstats['New']) $new = '<img src="'.$main_url.'css/img/icon-new.png" id=new></img>';
+
+	$kd_ratio = 0.0;
+	if($mapstats['Map Kills'] && $mapstats['Map Deaths'])
+		$kd_ratio = floatval($mapstats['Map Kills']) / floatval($mapstats['Map Deaths']);
+
+	foreach ($name_colors as $n_color) {
+	    $flags = str_split($mapstats['Flags']);
+	    $color_flags = str_split($n_color['flags']);
+	    if (count(array_intersect($flags, $color_flags)) == count($color_flags)) {
+	        $color = $n_color['color'];
+	        break;
+	    }
+	}
+
+	$user_url = ''.$main_url.'user.php?player='.$mapstats['Player'].'&me='.$player.'&style='.$style.'&top=15&show=1&default_order='.htmlspecialchars($_GET['default_order']).'&db_table1='.htmlspecialchars($_GET['db_table1']).'&db_table2='.htmlspecialchars($_GET['db_table2']).'&db_table3='.htmlspecialchars($_GET['db_table3']).'';
+	$new_user_url = str_replace(' ', '%20', $user_url);
+
+	if($color != $default_name_color) {
+		echo '<style>.glow'.$i.' { text-shadow: 1px 1px 6px '.$color.'; }</style>';
+	}
+
+	echo '
+		'.$tr.'
+		    '.$td.'
+		    <td>
+	            <div id=sp><div id=o'.$mapstats['Online'].'><div id='.$flag.'>'.$avatar.'<a href="'.$new_user_url.'" style="color:'.$color.'" class="glow'.$i.'">'.$mapstats['Nick'].'</a>'.$steam.''.$new.'</a></div></div></div>
+	            <td id=a><a>'.$mapstats['Map Kills'].'</a></td>
+	            <td id=a><a>'.$mapstats['Map Deaths'].'</td>
+	            <td id=a><a>'.number_format((float)$kd_ratio, 1, '.', '').'</td>
+	            <td id=a><a>'.$mapstats['Map Infections'].'</a></td>
+	            <td id="r'.$level.'"></td>
+		    </td>
+		</tr>
+	';
+	$i++;
+}?>
+</table>
+<?php
+$asset = isset($user['Map XP']) && $user['Map XP'] >= 0 ? '+' : '';
+
+echo '
+	<hr id="hr2" />
+		<div id="t5">
+			<table id="t3">
+			    <tr>
+			        <th id="th4">
+			            Your stats for<th id="th4"><a>this match</a></th>
+			            <tr>
+			                <td id="td2">
+			                    Total Kills
+			                    <td id="td3">
+			                        '.(isset($user['Map Kills']) ? $user['Map Kills'] : '').'
+			                        <tr>
+			                            <td id="td2">
+			                                Total Deaths
+			                                <td id="td3">
+			                                    '.(isset($user['Map Deaths']) ? $user['Map Deaths'] : '').'
+			                                    <tr>
+			                                        <td id="td2">
+			                                            Total XP Acquired
+			                                            <td id="td3">'.(isset($user['Map XP']) ? $asset.$user['Map XP'].' XP' : '').'</td>
+			                                        </td>
+			                                    </tr>
+			                                </td>
+			                            </td>
+			                        </tr>
+			                    </td>
+			                </td>
+			            </tr>
+			        </th>
+			    </tr>
+			</table>
+		</div>
+	</hr>
+';
+?>
